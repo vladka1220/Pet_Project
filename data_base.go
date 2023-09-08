bb9cb4ebd6065522cd5b3f72405bafe2581b2316
vladka1220 <34511278+vladka1220@users.noreply.github.com>
2023-09-08 01:35:40 +0100

    Update data_base.go

--- data_base.go
+++ data_base.go
@@ -1,1 +1,31 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+func database() {
+	// Строка подключения к базе данных PostgreSQL//****** данные БД (user и dbname)
+	connStr := "user=******* dbname=****** sslmode=disable"
+
+	// Открываем соединение с базой данных PostgreSQL
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Проверяем, что соединение работает
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Подключение к PostgreSQL успешно!")
+
+}
 
