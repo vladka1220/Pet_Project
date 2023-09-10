1d256fbc2a7a8b052be48f4819f14fdf3bdbbdf0
vladka1220 <34511278+vladka1220@users.noreply.github.com>
2023-09-10 17:49:52 +0100

    Update script.js

--- static/script.js
+++ static/script.js
@@ -3,8 +3,8 @@ document.getElementById("login-form").addEventListener("submit", function (event
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
     
-    // Проверяем учетные данные
-    if ((username === "admin" && password === "1") || (username === "vlad" && password === "123")) {
+    // Проверяем учетные данные, вместо **** можем добавить свои данные пока нет подключенной БД
+    if ((username === "****" && password === "*****") || (username === "*****" && password === "*****")) {
         // Перенаправляем на страницу продукции
         window.location.href = "product.html";
     } else {
